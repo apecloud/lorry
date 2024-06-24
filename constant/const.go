70a035f428f7cfef9d9d02c96c889588e52ede1f
xuriwuyun <xuriwuyun@gmail.com>
2024-06-24 18:04:58 +0800

    add codes

---
+++ constant/const.go
@@ -0,0 +1,276 @@
+/*
+Copyright (C) 2022-2023 ApeCloud Co., Ltd
+
+This file is part of KubeBlocks project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package constant
+
+const (
+	KBPrefix      = "KB"
+	KBLowerPrefix = "kb"
+)
+
+const (
+	KBServiceAccountName     = "KUBEBLOCKS_SERVICEACCOUNT_NAME"
+	KBToolsImage             = "KUBEBLOCKS_TOOLS_IMAGE"
+	KBImagePullPolicy        = "KUBEBLOCKS_IMAGE_PULL_POLICY"
+	KBDataScriptClientsImage = "KUBEBLOCKS_DATASCRIPT_CLIENTS_IMAGE"
+)
+
+const (
+	APIGroup = "kubeblocks.io"
+
+	AppName = "kubeblocks"
+
+	// K8S recommended well-known labels and annotation keys
+
+	// AppInstanceLabelKey refer cluster.Name
+	AppInstanceLabelKey = "app.kubernetes.io/instance"
+	// AppNameLabelKey refer clusterDefinition.Name before KubeBlocks Version 0.8.0 or refer ComponentDefinition.Name after KubeBlocks Version 0.8.0 (TODO：Pending)
+	AppNameLabelKey = "app.kubernetes.io/name"
+	// AppComponentLabelKey refer clusterDefinition.Spec.ComponentDefs[*].Name before KubeBlocks Version 0.8.0 or refer ComponentDefinition.Name after KubeBlocks Version 0.8.0
+	AppComponentLabelKey = "app.kubernetes.io/component"
+	// AppVersionLabelKey refer clusterVersion.Name before KubeBlocks Version 0.8.0 or refer ComponentDefinition.Name after KubeBlocks Version 0.8.0
+	AppVersionLabelKey   = "app.kubernetes.io/version"
+	AppManagedByLabelKey = "app.kubernetes.io/managed-by"
+	RegionLabelKey       = "topology.kubernetes.io/region"
+	ZoneLabelKey         = "topology.kubernetes.io/zone"
+
+	// kubeblocks.io labels
+	BackupProtectionLabelKey                 = "kubeblocks.io/backup-protection" // BackupProtectionLabelKey Backup delete protection policy label
+	AddonProviderLabelKey                    = "kubeblocks.io/provider"          // AddonProviderLabelKey marks the addon provider
+	RoleLabelKey                             = "kubeblocks.io/role"              // RoleLabelKey consensusSet and replicationSet role label key
+	ReadyWithoutPrimaryKey                   = "kubeblocks.io/ready-without-primary"
+	VolumeTypeLabelKey                       = "kubeblocks.io/volume-type"
+	ClusterAccountLabelKey                   = "account.kubeblocks.io/name"
+	KBAppClusterUIDLabelKey                  = "apps.kubeblocks.io/cluster-uid"
+	KBAppComponentLabelKey                   = "apps.kubeblocks.io/component-name"
+	KBAppShardingNameLabelKey                = "apps.kubeblocks.io/sharding-name"
+	KBAppComponentDefRefLabelKey             = "apps.kubeblocks.io/component-def-ref" // refer clusterDefinition.Spec.ComponentDefs[*].Name before KubeBlocks Version 0.8.0 or refer ComponentDefinition.Name after KubeBlocks Version 0.8.0
+	KBAppClusterDefTypeLabelKey              = "apps.kubeblocks.io/cluster-type"      // refer clusterDefinition.Spec.Type (deprecated)
+	AppConfigTypeLabelKey                    = "apps.kubeblocks.io/config-type"
+	KBManagedByKey                           = "apps.kubeblocks.io/managed-by" // KBManagedByKey marks resources that auto created
+	PVCNameLabelKey                          = "apps.kubeblocks.io/pvc-name"
+	VolumeClaimTemplateNameLabelKey          = "apps.kubeblocks.io/vct-name"
+	VolumeClaimTemplateNameLabelKeyForLegacy = "vct.kubeblocks.io/name" // Deprecated: only compatible with version 0.5, will be removed in 0.7
+	WorkloadTypeLabelKey                     = "apps.kubeblocks.io/workload-type"
+	ClassProviderLabelKey                    = "class.kubeblocks.io/provider"
+	ClusterDefLabelKey                       = "clusterdefinition.kubeblocks.io/name"
+	ClusterVerLabelKey                       = "clusterversion.kubeblocks.io/name"
+	ComponentDefinitionLabelKey              = "componentdefinition.kubeblocks.io/name"
+	CMConfigurationSpecProviderLabelKey      = "config.kubeblocks.io/config-spec"    // CMConfigurationSpecProviderLabelKey is ComponentConfigSpec name
+	CMConfigurationCMKeysLabelKey            = "config.kubeblocks.io/configmap-keys" // CMConfigurationCMKeysLabelKey Specify configmap keys
+	CMConfigurationTemplateNameLabelKey      = "config.kubeblocks.io/config-template-name"
+	CMTemplateNameLabelKey                   = "config.kubeblocks.io/template-name"
+	CMConfigurationTypeLabelKey              = "config.kubeblocks.io/config-type"
+	CMInsConfigurationHashLabelKey           = "config.kubeblocks.io/config-hash"
+	CMInsCurrentConfigurationHashLabelKey    = "config.kubeblocks.io/update-config-hash"
+	CMConfigurationConstraintsNameLabelKey   = "config.kubeblocks.io/config-constraints-name"
+	CMConfigurationTemplateVersion           = "config.kubeblocks.io/config-template-version"
+	ConsensusSetAccessModeLabelKey           = "cs.apps.kubeblocks.io/access-mode"
+	AddonNameLabelKey                        = "extensions.kubeblocks.io/addon-name"
+	OpsRequestTypeLabelKey                   = "ops.kubeblocks.io/ops-type"
+	OpsRequestNameLabelKey                   = "ops.kubeblocks.io/ops-name"
+	OpsRequestNamespaceLabelKey              = "ops.kubeblocks.io/ops-namespace"
+	ServiceDescriptorNameLabelKey            = "servicedescriptor.kubeblocks.io/name"
+	RestoreForHScaleLabelKey                 = "apps.kubeblocks.io/restore-for-hscale"
+	ResourceConstraintProviderLabelKey       = "resourceconstraint.kubeblocks.io/provider"
+
+	// StatefulSetPodNameLabelKey is used to mark the pod name of the StatefulSet
+	StatefulSetPodNameLabelKey = "statefulset.kubernetes.io/pod-name"
+
+	// kubeblocks.io annotations
+	ClusterSnapshotAnnotationKey                = "kubeblocks.io/cluster-snapshot"           // ClusterSnapshotAnnotationKey saves the snapshot of cluster.
+	DefaultClusterVersionAnnotationKey          = "kubeblocks.io/is-default-cluster-version" // DefaultClusterVersionAnnotationKey specifies the default cluster version.
+	OpsRequestAnnotationKey                     = "kubeblocks.io/ops-request"                // OpsRequestAnnotationKey OpsRequest annotation key in Cluster
+	ReconcileAnnotationKey                      = "kubeblocks.io/reconcile"                  // ReconcileAnnotationKey Notify k8s object to reconcile
+	RestartAnnotationKey                        = "kubeblocks.io/restart"                    // RestartAnnotationKey the annotation which notices the StatefulSet/DeploySet to restart
+	RestoreFromBackupAnnotationKey              = "kubeblocks.io/restore-from-backup"        // RestoreFromBackupAnnotationKey specifies the component to recover from the backup.
+	SnapShotForStartAnnotationKey               = "kubeblocks.io/snapshot-for-start"
+	ComponentReplicasAnnotationKey              = "apps.kubeblocks.io/component-replicas" // ComponentReplicasAnnotationKey specifies the number of pods in replicas
+	BackupPolicyTemplateAnnotationKey           = "apps.kubeblocks.io/backup-policy-template"
+	LastAppliedClusterAnnotationKey             = "apps.kubeblocks.io/last-applied-cluster"
+	PVLastClaimPolicyAnnotationKey              = "apps.kubeblocks.io/pv-last-claim-policy"
+	HaltRecoveryAllowInconsistentCVAnnotKey     = "clusters.apps.kubeblocks.io/allow-inconsistent-cv"
+	HaltRecoveryAllowInconsistentResAnnotKey    = "clusters.apps.kubeblocks.io/allow-inconsistent-resource"
+	PrimaryAnnotationKey                        = "rs.apps.kubeblocks.io/primary"
+	DisableUpgradeInsConfigurationAnnotationKey = "config.kubeblocks.io/disable-reconfigure"
+	LastAppliedConfigAnnotationKey              = "config.kubeblocks.io/last-applied-configuration"
+	LastAppliedOpsCRAnnotationKey               = "config.kubeblocks.io/last-applied-ops-name"
+	UpgradePolicyAnnotationKey                  = "config.kubeblocks.io/reconfigure-policy"
+	KBParameterUpdateSourceAnnotationKey        = "config.kubeblocks.io/reconfigure-source"
+	UpgradeRestartAnnotationKey                 = "config.kubeblocks.io/restart"
+	ConfigAppliedVersionAnnotationKey           = "config.kubeblocks.io/config-applied-version"
+	KubeBlocksGenerationKey                     = "kubeblocks.io/generation"
+	ExtraEnvAnnotationKey                       = "kubeblocks.io/extra-env"
+	LastRoleSnapshotVersionAnnotationKey        = "apps.kubeblocks.io/last-role-snapshot-version"
+	HostPortAnnotationKey                       = "kubeblocks.io/host-port"
+	HostPortIncludeAnnotationKey                = "network.kubeblocks.io/host-ports-include"
+	HostPortExcludeAnnotationKey                = "network.kubeblocks.io/host-ports-exclude"
+	DisableHAAnnotationKey                      = "kubeblocks.io/disable-ha"
+	// EnabledNodePortSvcAnnotationKey defines the feature gate of NodePort Service defined in ComponentDefinition.Spec.Services.
+	// Components defined in the annotation value, their all services of type NodePort defined in ComponentDefinition will be created; otherwise, they will be ignored.
+	// Multiple components are separated by ','. for example: "kubeblocks.io/enabled-node-port-svc: comp1,comp2"
+	EnabledNodePortSvcAnnotationKey = "kubeblocks.io/enabled-node-port-svc"
+	// EnabledPodOrdinalSvcAnnotationKey defines the feature gate of PodOrdinal Service defined in ComponentDefinition.Spec.Services.
+	// Components defined in the annotation value, their all Services defined in the ComponentDefinition with the GeneratePodOrdinalService attribute set to true will be created; otherwise, they will be ignored.
+	// This can generate a corresponding Service for each Pod, which can be used in certain specific scenarios: for example, creating a dedicated access service for each read-only Pod.
+	// Multiple components are separated by ','. for example: "kubeblocks.io/enabled-pod-ordinal-svc: comp1,comp2"
+	EnabledPodOrdinalSvcAnnotationKey = "kubeblocks.io/enabled-pod-ordinal-svc"
+	// DisabledClusterIPSvcAnnotationKey defines whether the feature gate of ClusterIp Service defined in ComponentDefinition.Spec.Services will be effected.
+	// Components defined in the annotation value, their all services of type ClusterIp defined in ComponentDefinition will be ignored; otherwise, they will be created.
+	// Multiple components are separated by ','. for example: "kubeblocks.io/disabled-cluster-ip-svc: comp1,comp2"
+	DisabledClusterIPSvcAnnotationKey = "kubeblocks.io/disabled-cluster-ip-svc"
+	// ShardSvcAnnotationKey defines the feature gate of creating service for each shard.
+	// Sharding name defined in the annotation value, a set of Service defined in Cluster.Spec.Services with the ShardingSelector will be automatically generated for each shard when Cluster.Spec.ShardingSpecs[x].shards is not nil.
+	// Multiple sharding names are separated by ','. for example: "kubeblocks.io/enabled-shard-svc: proxy-shard,db-shard"
+	ShardSvcAnnotationKey = "kubeblocks.io/enabled-shard-svc"
+
+	// kubeblocks.io well-known finalizers
+	DBClusterFinalizerName             = "cluster.kubeblocks.io/finalizer"
+	DBComponentFinalizerName           = "component.kubeblocks.io/finalizer"
+	DBComponentDefinitionFinalizerName = "componentdefinition.kubeblocks.io/finalizer"
+	ConfigurationTemplateFinalizerName = "config.kubeblocks.io/finalizer"
+	ServiceDescriptorFinalizerName     = "servicedescriptor.kubeblocks.io/finalizer"
+	OpsRequestFinalizerName            = "opsrequest.kubeblocks.io/finalizer"
+
+	// ConfigurationTplLabelPrefixKey clusterVersion or clusterdefinition using tpl
+	ConfigurationTplLabelPrefixKey         = "config.kubeblocks.io/tpl"
+	ConfigurationConstraintsLabelPrefixKey = "config.kubeblocks.io/constraints"
+
+	// CMInsLastReconfigurePhaseKey defines the current phase
+	CMInsLastReconfigurePhaseKey = "config.kubeblocks.io/last-applied-reconfigure-phase"
+
+	// ConfigurationRevision defines the current revision
+	// TODO support multi version
+	ConfigurationRevision          = "config.kubeblocks.io/configuration-revision"
+	LastConfigurationRevisionPhase = "config.kubeblocks.io/revision-reconcile-phase"
+
+	// Deprecated: only compatible with version 0.6, will be removed in 0.8
+	// CMInsEnableRerenderTemplateKey is used to enable rerender template
+	CMInsEnableRerenderTemplateKey = "config.kubeblocks.io/enable-rerender"
+
+	// IgnoreResourceConstraint is used to specify whether to ignore the resource constraint
+	IgnoreResourceConstraint = "resource.kubeblocks.io/ignore-constraint"
+
+	RBACRoleName        = "kubeblocks-cluster-pod-role"
+	RBACClusterRoleName = "kubeblocks-volume-protection-pod-role"
+
+	// FeatureReconciliationInCompactModeAnnotationKey indicates that the controller should run in compact mode,
+	// means to try the best to cutoff useless objects.
+	FeatureReconciliationInCompactModeAnnotationKey = "kubeblocks.io/compact-mode"
+)
+
+const (
+	// ReasonNotFoundCR referenced custom resource not found
+	ReasonNotFoundCR = "NotFound"
+	// ReasonRefCRUnavailable  referenced custom resource is unavailable
+	ReasonRefCRUnavailable = "Unavailable"
+	// ReasonDeletedCR deleted custom resource
+	ReasonDeletedCR = "DeletedCR"
+	// ReasonDeletingCR deleting custom resource
+	ReasonDeletingCR = "DeletingCR"
+	// ReasonCreatedCR created custom resource
+	ReasonCreatedCR = "CreatedCR"
+	// ReasonRunTaskFailed run task failed
+	ReasonRunTaskFailed = "RunTaskFailed"
+	// ReasonDeleteFailed delete failed
+	ReasonDeleteFailed = "DeleteFailed"
+)
+
+const (
+	// Container port name
+	LorryHTTPPortName                  = "lorry-http-port"
+	LorryGRPCPortName                  = "lorry-grpc-port"
+	LorryContainerName                 = "lorry"
+	LorryInitContainerName             = "init-lorry"
+	ProbeInitContainerName             = "kb-initprobe"
+	RoleProbeContainerName             = "kb-checkrole"
+	StatusProbeContainerName           = "kb-checkstatus"
+	RunningProbeContainerName          = "kb-checkrunning"
+	VolumeProtectionProbeContainerName = "kb-volume-protection"
+	LorryRoleProbePath                 = "/v1.0/checkrole"
+	LorryVolumeProtectPath             = "/v1.0/volumeprotection"
+
+	// the filedpath name used in event.InvolvedObject.FieldPath
+	ProbeCheckStatusPath  = "spec.containers{" + StatusProbeContainerName + "}"
+	ProbeCheckRunningPath = "spec.containers{" + RunningProbeContainerName + "}"
+)
+
+const (
+	ConfigSidecarName        = "config-manager"
+	ConfigManagerGPRCPortEnv = "CONFIG_MANAGER_GRPC_PORT"
+	ConfigManagerLogLevel    = "CONFIG_MANAGER_LOG_LEVEL"
+
+	PodMinReadySecondsEnv = "POD_MIN_READY_SECONDS"
+	ConfigTemplateType    = "tpl"
+	ConfigInstanceType    = "instance"
+
+	ReconfigureManagerSource  = "manager"
+	ReconfigureUserSource     = "ops"
+	ReconfigureTemplateSource = "external-template"
+
+	ConfigManagerPortName = "config-manager"
+)
+
+const (
+	Primary   = "primary"
+	Secondary = "secondary"
+
+	Leader   = "leader"
+	Follower = "follower"
+	Learner  = "learner"
+)
+
+// username and password are keys in created secrets for others to refer to.
+const (
+	AccountNameForSecret   = "username"
+	AccountPasswdForSecret = "password"
+)
+
+const (
+	FeatureGateReplicatedStateMachine = "REPLICATED_STATE_MACHINE" // enable rsm
+)
+
+const (
+	KubernetesClusterDomainEnv = "KUBERNETES_CLUSTER_DOMAIN"
+	DefaultDNSDomain           = "cluster.local"
+)
+
+const (
+	ServiceDescriptorUsernameKey = "username"
+	ServiceDescriptorPasswordKey = "password"
+	ServiceDescriptorEndpointKey = "endpoint"
+	ServiceDescriptorPortKey     = "port"
+)
+
+const (
+	BackupNameKeyForRestore           = "name"
+	BackupNamespaceKeyForRestore      = "namespace"
+	VolumeRestorePolicyKeyForRestore  = "volumeRestorePolicy"
+	DoReadyRestoreAfterClusterRunning = "doReadyRestoreAfterClusterRunning"
+	RestoreTimeKeyForRestore          = "restoreTime"
+	ConnectionPassword                = "connectionPassword"
+)
+
+const (
+	KBAppMultiClusterPlacementKey = "apps.kubeblocks.io/multi-cluster-placement"
+)
+
+const (
+	KBGeneratedVirtualCompDefPrefix = "KB_GENERATED_VIRTUAL_COMP_DEF"
+)
---
+++ go.mod
@@ -0,0 +1,158 @@
+module github.com/apecloud/lorry
+
+go 1.22.0
+
+toolchain go1.22.4
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/apecloud/kubeblocks v0.8.3
+	github.com/deckarep/golang-set/v2 v2.6.0
+	github.com/dlclark/regexp2 v1.11.0
+	github.com/go-logr/logr v1.4.2
+	github.com/go-logr/zapr v1.3.0
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/golang/mock v1.6.0
+	github.com/jackc/pgx/v5 v5.6.0
+	github.com/mitchellh/mapstructure v1.5.1-0.20220423185008-bf980b35cac4
+	github.com/onsi/ginkgo/v2 v2.19.0
+	github.com/onsi/gomega v1.33.1
+	github.com/pashagolub/pgxmock/v2 v2.12.0
+	github.com/pkg/errors v0.9.1
+	github.com/redis/go-redis/v9 v9.5.3
+	github.com/shirou/gopsutil/v3 v3.24.5
+	github.com/spf13/afero v1.11.0
+	github.com/spf13/cast v1.6.0
+	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.9.0
+	go.etcd.io/etcd/client/v3 v3.5.14
+	go.etcd.io/etcd/server/v3 v3.5.14
+	go.mongodb.org/mongo-driver v1.15.1
+	go.uber.org/zap v1.27.0
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
+	k8s.io/api v0.30.2
+	k8s.io/apimachinery v0.30.2
+	k8s.io/client-go v0.30.2
+	sigs.k8s.io/controller-runtime v0.18.4
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/pprof v0.0.0-20240424215950-a892ee059fd6 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.1-0.20210315223345-82c243799c99 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/hashicorp/hcl v1.0.1-vault-5 // indirect
+	github.com/imdario/mergo v0.3.14 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jonboulle/clockwork v0.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/soheilhy/cmux v0.1.5 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20220101234140-673ab2c3ae75 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.etcd.io/bbolt v1.3.10 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/v2 v2.305.14 // indirect
+	go.etcd.io/etcd/pkg/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/raft/v3 v3.5.14 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.20.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.20.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.20.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20240213162025-012b6fc9bca9 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240311132316-a219d84964c2 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240314234333-6e1732d8331c // indirect
+	google.golang.org/grpc v1.62.1 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apiextensions-apiserver v0.30.1 // indirect
+	k8s.io/klog/v2 v2.120.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
